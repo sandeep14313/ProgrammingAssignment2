dfacb59bc257b17bb0dc2162105c7b39a8c32288
Sandeep Nandi <54443601+sandeep14313@users.noreply.github.com>
2020-04-30 12:31:15 +0530

    Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -5,16 +5,21 @@
 
 makeCacheMatrix <- function(x = matrix()) 
 {
-  inv <- NULL
+  # cached inverse of matrix
+  inv <- NULL  
+  ## get and set for matrix
   get <- function() x
   set <- function(y) 
   {
     x <<- y
     inv <<- NULL
   }
+  
+  ## get and set for matrix inverse
   getinv <- function() inv
   setinv <- function(inverse) inv <<- inverse
   
+  ## return list of functions for matrix
   list(get=get, set=set, getinv=getinv, setinv=setinv)
 }
 
@@ -27,10 +32,14 @@ cacheSolve <- function(x, ...)
   if (!is.null(inv)) 
   {
     message("inverse is cached")
+    #returns cached matrix inverse if already computed
     return(inv)
   }
+  # compute inverse of matrix 
   m <- x$get()
   inv <- solve(m, ...)
-  x$setinv(inv)
+  # cache inverse
+  x$setinv(inv)  
+  # returns the inverse of the matrix
   return(inv)
 }
